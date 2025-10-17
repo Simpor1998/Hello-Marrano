6caa3c0f243950126591cdb2f61b811c7041a2da
Simpor <andresdaa@hotmail.com>
2025-10-17 02:52:24 -0500

    ultima linea poe el dia de hoy

--- hellogit.py
+++ hellogit.py
@@ -1,3 +1,5 @@
 print("New Hello GitHUB, AGREGO UN ESPACIO PARA VERIFICAR EN EL ADD. COMMIT Y LUEGO EL PUSH A LA RAMA ORIGIN MAIN")
 
 print("AGREGO UNA LINEA MAS PARA BUSCAR EL ADD Y AGRERGAR")
+
+print("que meto otra linea para ver que pasa con el add y el commit")
