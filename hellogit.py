5dc297097d5ad07815a3bb3fcb7650796712c7a9
Simpor <andresdaa@hotmail.com>
2025-10-17 03:22:00 -0500

    aqui ya se borro la rama de nueas prueba y se hizo el merge con la rama main y ahora voy a hacer un pull request para ver que pasa

--- hellogit.py
+++ hellogit.py
@@ -7,3 +7,5 @@ print("que meto otra linea para ver que pasa con el add y el commit")
 print("LO HAGO PARA PROBAR LA NUEA RAMA DE PRUEBA Y DESPUESTA HACERLE EL MERGE")
 
 print("agrego este para probar el pull request y el merge de la rama prueba")
+
+print("aqui ya se borro la rama de nueas prueba y se hizo el merge con la rama main y ahora voy a hacer un pull request para ver que pasa")
